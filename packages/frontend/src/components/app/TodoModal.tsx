c96198114f7db93a1f4efb6b8afd6850d6497128
Kyle Pfromer <kpfromer2@gmail.com>
2020-08-12 19:49:12 -0600

    fix: update import

--- packages/frontend/src/components/app/TodoModal.tsx
+++ packages/frontend/src/components/app/TodoModal.tsx
@@ -4,7 +4,7 @@ import {
   TodoFieldsFragment,
   ProjectFieldsFragment
 } from '../../generated/types-and-hooks';
-import { Modal } from '../useModal';
+import { Modal } from '../Modal';
 import { Flex, Box, Text } from 'rebass';
 import { Label, Checkbox, Input } from '@rebass/forms';
 import { useForm } from 'react-hook-form';
