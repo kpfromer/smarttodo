e2fac6fdd7fae25cd4817123a3b0eb5945de05d1
Kyle Pfromer <kpfromer2@gmail>
2020-09-05 20:48:27 -0600

    chore: add todo tests for frontend todo page

---
+++ packages/frontend/src/components/app/__tests__/todo.spec.tsx
@@ -0,0 +1,7 @@
+describe('todo page', () => {
+  it.todo('creates a todo');
+  it.todo("updates a todo's name");
+  it.todo('deletes a todo');
+  it.todo('completes a todo');
+  it.todo('changes projects');
+});
