f0d1ad0ae7c5120ce95c9d66bb57fc50381dea1d
Kyle Pfromer <kpfromer2@gmail.com>
2020-12-04 19:09:13 -0700

    chore: move jest and nodemon config to package.json

--- packages/backend/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
-  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-  testEnvironment: 'node',
-};
